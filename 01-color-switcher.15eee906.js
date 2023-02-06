8455b81a8c2da6769f2cfa40e7edef66faaa7852
Sforka <112426543+Sforka@users.noreply.github.com>
2023-02-06 17:50:38 +0000

    Deploying to gh-pages from @ Sforka/goit-js-hw-09@7dba9792244fa738d346003d5699c58e199039b8 🚀

---
+++ 01-color-switcher.15eee906.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),o=document.querySelector("body");e.addEventListener("click",function(){setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3);e.disabled=!0}()),t.addEventListener("click",(clearTimeout(changeColor),void(e.disabled=!1)))}();
+//# sourceMappingURL=01-color-switcher.15eee906.js.map
